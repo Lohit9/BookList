d51e278f65a51802cd2a254739ae6aafbec4a20d
Lohit Talasila <Lohit9@users.noreply.github.com>
2015-09-09 10:22:37 -0400

    added updated files

---
+++ AwesomeProject/Name.js
@@ -0,0 +1,42 @@
+'use strict';
+
+ var React = require('react-native');
+
+ var
+ {
+   StyleSheet,
+   View,
+   Text,
+   Component
+ } = React;
+
+ var styles = StyleSheet.create({
+   description:{
+     fontSize: 20,
+     backgroundColor: 'white'
+
+   },
+   container:{
+     flex:1,
+     justifyContent: 'center',
+     alignItems: 'center'
+
+   }
+
+
+ });
+
+ class Name extends Component
+ {
+   render() {
+       return (
+       <View style={styles.container}>
+         <Text style={styles.description}>
+           Name Tab
+         </Text>
+     </View>
+       );
+   }
+ }
+
+module.exports = Name;
